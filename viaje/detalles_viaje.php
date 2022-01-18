ddd29a7d013942c8ec0fd640c62ff7948e7bae67
AaronHDN <62114009+AaronHDN@users.noreply.github.com>
2022-01-19 00:41:58 +0100

    a

--- viaje/detalles_viaje.php
+++ viaje/detalles_viaje.php
@@ -31,10 +31,7 @@
 <h3>Contactar con el conductor: </h3>
 
 <?php
-var_dump($conductor);
-var_dump($dataConversaciones->data->usuarios);
-var_dump(in_array($conductor, $dataConversaciones->data->usuarios));
-    if (in_array($conductor->_id, $dataConversaciones->data->usuarios->_id)) { ?>
+    if (in_array($conductor, $dataConversaciones->data->usuarios)) { ?>
             <form action="./mensajeria/ver_conversacion.php" method="GET">
                 <input type="hidden" value="<?php echo $conductor->_id?>" name="id_ajeno">
                 <input type="hidden" value="<?php echo $_GET['id_local']?>" name="id_local">
