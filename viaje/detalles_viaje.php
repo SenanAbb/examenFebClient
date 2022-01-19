36a282454f499bf0706db9ef816a7f1c06feba55
AaronHDN <62114009+AaronHDN@users.noreply.github.com>
2022-01-19 01:00:02 +0100

    a

--- viaje/detalles_viaje.php
+++ viaje/detalles_viaje.php
@@ -40,7 +40,7 @@
 
         <?php } else { ?>
             <form action="../mensajeria/crear_conversacion.php" method="POST">
-                <input type="hidden" value="<?php echo $conductor->_id?>" name="id_ajeno">
+                <input type="hidden" value="<?php echo $conductor->_id?>" name="select">
                 <input type="hidden" value="<?php echo $_GET['id_local']?>" name="id_local">
                 <input type="submit" value="Empezar conversación con el conductor">
             </form>
