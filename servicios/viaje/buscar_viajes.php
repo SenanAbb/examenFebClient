3649b35654d4bb5962f6efe46dc9724659854471
EloxZ <45355879+EloxZ@users.noreply.github.com>
2022-02-07 17:20:20 +0100

    Update buscar_viajes.php

--- servicios/viaje/buscar_viajes.php
+++ servicios/viaje/buscar_viajes.php
@@ -2,20 +2,20 @@
 
 session_start();
 
-echo "Hola";
-
 $origen = trim($_GET['origen']);
 $destino = trim($_GET['destino']);
 $fecha = strtotime($_GET['fecha']);
 
-echo $origen;
-echo $destino;
-echo $fecha;
-
 $res = file_get_contents("http://blablacariw.herokuapp.com/travels?origen=" . $origen . "&destino=" . $destino);
-$data = json_decode($res, true);
-var_dump($data);
-echo "\n";
-var_dump($data['data']['viajes']);
-//header('Location: ../../index.php');
+$data = json_decode($res);
+$viajes = array();
+
+foreach ($data->data->viajes as $viaje){
+    //if (!empty($fecha) && $fecha === $viaje->fecha_salida){
+        array_push($viajes, $viaje);
+    //}
+}
+
+$_SESSION['viajes_encontrados'] = $viajes;
+header('Location: ../../index.php');
 ?>
